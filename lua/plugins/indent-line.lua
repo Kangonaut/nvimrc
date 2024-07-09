4ea51acbc7e4a090ddddafc6bfac822d8370f94f
Kangonaut <120662571+Kangonaut@users.noreply.github.com>
2024-07-09 10:38:17 +0200

    add indent-line

---
+++ lua/plugins/indent-line.lua
@@ -0,0 +1,11 @@
+-- [[ indent-blankline.nvim ]]
+-- desc: add indentation guides
+-- repo: https://github.com/lukas-reineke/indent-blankline.nvim
+
+return {
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    main = 'ibl',
+    opts = {},
+  },
+}
