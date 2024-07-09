714d1ae62decd51659e556953b8e40b7ffb88734
Kangonaut <120662571+Kangonaut@users.noreply.github.com>
2024-07-09 13:50:05 +0200

    fix bufferline when restoring session

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -26,8 +26,18 @@ return {
   config = function(_, opts)
     require("bufferline").setup(opts)
 
-    -- [[ configure keymaps ]]
+    -- fix bufferline when restoring a session
+    vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
+      callback = function()
+        vim.schedule(
+          function()
+            pcall(nvim_bufferline)
+          end
+        )
+      end,
+    })
 
+    -- [[ configure keymaps ]]
     -- cycling buffers
     vim.keymap.set("n", "[b", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev Buffer" })
     vim.keymap.set("n", "]b", "<cmd>BufferLineCycleNext<cr>", { desc = "Next Buffer" })
