c5b84845e347e784b8707dfab83e98f81dcab131
Kangonaut <120662571+Kangonaut@users.noreply.github.com>
2024-07-09 13:44:18 +0200

    add vim-sleuth

---
+++ lua/plugins/vim-sleuth.lua
@@ -0,0 +1,9 @@
+-- [[ vim-sleuth ]]
+-- desc: heuristically set buffer options
+-- repo: https://github.com/tpope/vim-sleuth
+
+-- INFO: automatically adjusts `shiftwidth` and `expandtab` based on the curren file
+
+return {
+  'tpope/vim-sleuth',
+}
