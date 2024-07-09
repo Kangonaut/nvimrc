c59d5ca2184832688e56f640403bfa150f7d16c5
Kangonaut <120662571+Kangonaut@users.noreply.github.com>
2024-07-09 14:08:35 +0200

    add leetcode.nvim

---
+++ lua/plugins/leetcode.lua
@@ -0,0 +1,30 @@
+-- [[ leetcode.nvim ]]
+-- desc: solve LeetCode problems inside Neovim
+-- repo: https://github.com/kawre/leetcode.nvim
+
+local arg = "leetcode.nvim"
+
+return {
+  "kawre/leetcode.nvim",
+
+  -- NOTE: loads the plugin on startup if nvim is called like this: `nvim leetcode.nvim`
+  lazy = arg ~= vim.fn.argv()[1],
+
+  build = ":TSUpdate html",
+  dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+
+      -- optional
+      "nvim-treesitter/nvim-treesitter",
+      "rcarriga/nvim-notify",
+      "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    arg = arg,
+
+    ---@type lc.lang
+    lang = "python3",
+  },
+}
