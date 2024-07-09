c8de43735a9116df9c9f622edf299b2cc630a525
Kangonaut <120662571+Kangonaut@users.noreply.github.com>
2024-07-09 09:30:53 +0200

    add neo-tree

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,27 @@
+-- [[ neo-tree ]]
+-- desc: used to display a file tree
+-- repo: https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  cmd = 'Neotree',
+  keys = {
+    -- NOTE: toggle the neo-tree window using backslash "\"
+    { '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
+  },
+  opts = {
+    filesystem = {
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
+  },
+}
