9666865e4fafb9392f4a1567522f35a3e0629401
Kangonaut <120662571+Kangonaut@users.noreply.github.com>
2024-07-09 10:33:55 +0200

    add todo-comments

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,15 @@
+-- [[ todo-comments.nvim ]]
+-- desc: highlight TODO (and similar) comments
+-- repo: https://github.com/folke/todo-comments.nvim
+
+-- NOTE: this is a example
+return {
+  'folke/todo-comments.nvim', 
+  event = 'VimEnter', 
+  dependencies = { 
+    'nvim-lua/plenary.nvim' 
+  }, 
+  opts = { 
+    signs = false 
+  } 
+}
