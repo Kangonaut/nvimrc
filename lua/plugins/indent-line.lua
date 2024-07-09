5a663cffc1bd234e5ddc5e6c60ddd52e90ad2d9c
Kangonaut <120662571+Kangonaut@users.noreply.github.com>
2024-07-09 13:44:50 +0200

    edit indent-line to use ASCII pipe symbol

--- lua/plugins/indent-line.lua
+++ lua/plugins/indent-line.lua
@@ -6,6 +6,15 @@ return {
   {
     'lukas-reineke/indent-blankline.nvim',
     main = 'ibl',
-    opts = {},
+    opts = {
+      indent = {
+        char = "|",
+        tab_char = "|", 
+      },
+      scope = {
+        show_start = false,
+        show_end = false,
+      }
+    },
   },
 }
